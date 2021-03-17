d5926cc23b77760e809edaa74dc4df45d38d9dc1
Dakouri Kobri <maurille.kobri@gmail.com>
2021-03-17 14:23:07 +0100

    Modify the App class to suit the ToDo-List Main class

--- app/src/main/java/SDA9/DakouriKobri/ToDoList/App.java
+++ app/src/main/java/SDA9/DakouriKobri/ToDoList/App.java
@@ -3,12 +3,23 @@
  */
 package SDA9.DakouriKobri.ToDoList;
 
+/**
+ * The class App contains the main method by which the application starts running.
+ *
+ * @author Dakouri Maurille-Constant Kobri
+ * @version 1.0
+ * @since 2021.03.09
+ */
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    /**
+     * The main method, which is starting point of the application.
+     *
+     * @param args array of characters sequence passed to the main method.
+     */
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
 }
